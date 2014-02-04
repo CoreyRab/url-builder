64d93cef884841ba2e949d0865c431fa657420a7
Corey Rabazinski <corey@codeschool.com>
2014-02-04 14:34:50 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+var myApp = angular.module('myApp', []);
+
+app.filter('nospace', function () {
+    return function (value) {
+        var str = value.replace(/\s+/g, '');
+        return str;
+    };
+});
+
+myApp.factory('Data', function(){
+
+
+
+});
+
+app.run(function($rootScope,$window){
+
+   $rootScope.prompt = $window.prompt;
+});
